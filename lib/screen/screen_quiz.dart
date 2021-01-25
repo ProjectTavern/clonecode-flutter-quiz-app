d75c5acac5579c6094164ea3a55c4f9c29d8ab78
winterguard <aliasbonfire@gmail.com>
2021-01-25 23:11:56 +0900

    퀴즈 화면 구현

--- lib/screen/screen_quiz.dart
+++ lib/screen/screen_quiz.dart
@@ -1,5 +1,7 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz_app/model/model_quiz.dart';
+import 'package:flutter_quiz_app/widget/widget_candidate.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
 class QuizScreen extends StatefulWidget {
@@ -47,31 +49,78 @@ class _QuizScreenState extends State<QuizScreen> {
 
   Widget _buildQuizCard(Quiz quiz, double width, double height) {
     return Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          border: Border.all(color: Colors.white),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: Colors.white),
+        color: Colors.white,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(0, width * 0.024, 0, width * 0.024),
+            child: Text(
+              'Q' + (_currentIndex + 1).toString() + '.',
+              style: TextStyle(
+                fontSize: width * 0.06,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            width: width * 0.8,
+            padding: EdgeInsets.only(top: width * 0.012),
+            child: AutoSizeText(
+              quiz.title,
+              textAlign: TextAlign.center,
+              maxLines: 2,
+              style: TextStyle(
+                fontSize: width * 0.048,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(),
+          ),
+          Column(
+            children: _buildCandidates(width, quiz),
+          )
+        ],
+      ),
+    );
+  }
+
+  List<Widget> _buildCandidates(double width, Quiz quiz) {
+    List<Widget> _children = [];
+
+    for (int i = 0; i < 4; i++) {
+      _children.add(
+        CandWidget(
+          index: i,
+          text: quiz.candidates[i],
+          width: width,
+          answerState: _answerState[i],
+          tap: () {
+            setState(() {
+              for (int j = 0; j < 4; j++) {
+                if (j == i) {
+                  _answerState[j] = true;
+                  _answers[_currentIndex] = j;
+                } else {
+                  _answerState[j] = false;
+                }
+              }
+            });
+          },
         ),
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                padding:
-                    EdgeInsets.fromLTRB(0, width * 0.024, 0, width * 0.024),
-                child: Text(
-                  'Q' + (_currentIndex + 1).toString() + '.',
-                  style: TextStyle(
-                    fontSize: width * 0.06,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Container(
-                width: width * 0.8,
-                padding: EdgeInsets.only(top: width * 0.012),
-                child: Text(
-                  quiz.title,
-                ),
-              ),
-            ]));
+      );
+      _children.add(
+        Padding(
+          padding: EdgeInsets.all(width * 0.024),
+        ),
+      );
+    }
+    return _children;
   }
 }
