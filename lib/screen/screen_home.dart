f1e17bc7d7280f41ce3be53b03cd4acbf100bcf8
winterguard <aliasbonfire@gmail.com>
2021-01-25 15:49:53 +0900

    홈 화면 구성

--- lib/screen/screen_home.dart
+++ lib/screen/screen_home.dart
@@ -46,9 +46,60 @@ class _HomeScreenState extends State<HomeScreen> {
             Padding(
               padding: EdgeInsets.all(width * 0.048),
             ),
+            _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
+            _buildStep(width, '2. 문제를 잘 읽고 정답을 고른 뒤, 다음 문제 버튼을 눌러주세요.'),
+            _buildStep(width, '3. 만점을 향해 도전해보세요!'),
+            Padding(
+              padding: EdgeInsets.all(width * 0.048),
+            ),
+            Container(
+              padding: EdgeInsets.only(bottom: width * 0.036),
+              child: Center(
+                child: ButtonTheme(
+                  minWidth: width * 0.8,
+                  height: height * 0.05,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: RaisedButton(
+                    child: Text(
+                      '지금 퀴즈 풀기',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    color: Colors.deepPurple,
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
+  }
+
+  Widget _buildStep(double width, String title) {
+    return Container(
+        padding: EdgeInsets.fromLTRB(
+          width * 0.048,
+          width * 0.024,
+          width * 0.048,
+          width * 0.024,
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Icon(
+              Icons.check_box,
+              size: width * 0.04,
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                right: width * 0.024,
+              ),
+            ),
+            Text(title),
+          ],
+        ));
   }
 }
