328f21b00f0189244b1de516af6eb76bf63f75d8
winterguard <aliasbonfire@gmail.com>
2021-01-30 15:27:49 +0900

    퀴즈앱 스와이프 안되도록 감싸기

--- lib/screen/screen_result.dart
+++ lib/screen/screen_result.dart
@@ -24,92 +24,94 @@ class ResultScreen extends StatelessWidget {
 
     return WillPopScope(
       onWillPop: () async => false,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("My Quiz App"),
-          backgroundColor: Colors.deepPurple,
-          leading: Container(),
-        ),
-        body: Center(
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              border: Border.all(color: Colors.deepPurple),
-              color: Colors.deepPurple,
-            ),
-            width: width * 0.85,
-            height: height * 0.5,
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(bottom: width * 0.048),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(color: Colors.deepPurple),
-                    color: Colors.white,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("My Quiz App"),
+            backgroundColor: Colors.deepPurple,
+            leading: Container(),
+          ),
+          body: Center(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(color: Colors.deepPurple),
+                color: Colors.deepPurple,
+              ),
+              width: width * 0.85,
+              height: height * 0.5,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(bottom: width * 0.048),
                   ),
-                  width: width * 0.73,
-                  height: height * 0.25,
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.fromLTRB(
-                            0, width * 0.048, 0, width * 0.012),
-                        child: Text(
-                          '수고하셨습니다!',
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(color: Colors.deepPurple),
+                      color: Colors.white,
+                    ),
+                    width: width * 0.73,
+                    height: height * 0.25,
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.fromLTRB(
+                              0, width * 0.048, 0, width * 0.012),
+                          child: Text(
+                            '수고하셨습니다!',
+                            style: TextStyle(
+                              fontSize: width * 0.055,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          '당신의 점수는',
                           style: TextStyle(
-                            fontSize: width * 0.055,
+                            fontSize: width * 0.048,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                      ),
-                      Text(
-                        '당신의 점수는',
-                        style: TextStyle(
-                          fontSize: width * 0.048,
-                          fontWeight: FontWeight.bold,
+                        Expanded(
+                          child: Container(),
                         ),
-                      ),
-                      Expanded(
-                        child: Container(),
-                      ),
-                      Text(
-                        score.toString() + '/' + quizs.length.toString(),
-                        style: TextStyle(
-                          fontSize: width * 0.21,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.red,
+                        Text(
+                          score.toString() + '/' + quizs.length.toString(),
+                          style: TextStyle(
+                            fontSize: width * 0.21,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: Container(),
-                ),
-                Container(
-                  padding: EdgeInsets.only(bottom: width * 0.048),
-                  child: ButtonTheme(
-                      minWidth: width * 0.73,
-                      height: height * 0.05,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: RaisedButton(
-                        onPressed: () {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) {
-                            return HomeScreen();
-                          }));
-                        },
-                        child: Text("다시 하기"),
-                        color: Colors.white,
-                        textColor: Colors.black,
-                      )),
-                ),
-              ],
+                  Expanded(
+                    child: Container(),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(bottom: width * 0.048),
+                    child: ButtonTheme(
+                        minWidth: width * 0.73,
+                        height: height * 0.05,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: RaisedButton(
+                          onPressed: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              return HomeScreen();
+                            }));
+                          },
+                          child: Text("다시 하기"),
+                          color: Colors.white,
+                          textColor: Colors.black,
+                        )),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
