586b8c010d2a3f6169f8811e7539d93a632d723d
winterguard <aliasbonfire@gmail.com>
2021-01-30 15:23:36 +0900

    결과 페이지

--- lib/screen/screen_result.dart
+++ lib/screen/screen_result.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import 'package:flutter_quiz_app/model/model_quiz.dart';
+import 'package:flutter_quiz_app/screen/screen_home.dart';
 
 class ResultScreen extends StatelessWidget {
   List<int> answers;
@@ -21,7 +22,8 @@ class ResultScreen extends StatelessWidget {
       }
     }
 
-    return SafeArea(
+    return WillPopScope(
+      onWillPop: () async => false,
       child: Scaffold(
         appBar: AppBar(
           title: Text("My Quiz App"),
@@ -38,7 +40,76 @@ class ResultScreen extends StatelessWidget {
             width: width * 0.85,
             height: height * 0.5,
             child: Column(
-              children: <Widget>[],
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(bottom: width * 0.048),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.deepPurple),
+                    color: Colors.white,
+                  ),
+                  width: width * 0.73,
+                  height: height * 0.25,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.fromLTRB(
+                            0, width * 0.048, 0, width * 0.012),
+                        child: Text(
+                          '수고하셨습니다!',
+                          style: TextStyle(
+                            fontSize: width * 0.055,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        '당신의 점수는',
+                        style: TextStyle(
+                          fontSize: width * 0.048,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      Text(
+                        score.toString() + '/' + quizs.length.toString(),
+                        style: TextStyle(
+                          fontSize: width * 0.21,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Container(),
+                ),
+                Container(
+                  padding: EdgeInsets.only(bottom: width * 0.048),
+                  child: ButtonTheme(
+                      minWidth: width * 0.73,
+                      height: height * 0.05,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: RaisedButton(
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return HomeScreen();
+                          }));
+                        },
+                        child: Text("다시 하기"),
+                        color: Colors.white,
+                        textColor: Colors.black,
+                      )),
+                ),
+              ],
             ),
           ),
         ),
